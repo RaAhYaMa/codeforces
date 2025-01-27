1aadea7154df92177e20436b131b651f31af36db
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-01-27 11:20:05 +0700

    Finish Watermelon problem

---
+++ Watermelon/test.java
@@ -0,0 +1,63 @@
+package Watermelon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class test {
+    private static InputReader in;
+    private static PrintWriter out;
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        in = new InputReader(inputStream);
+        out = new PrintWriter(outputStream);
+
+        int x = in.nextInt();
+	if (x % 2 == 0 && x >= 4) {
+	    out.println("YES");
+	}
+	else {
+	    out.println("NO");
+	}
+
+	out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public char nextChar() {
+            return next().charAt(0);
+        }
+
+        public Long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
