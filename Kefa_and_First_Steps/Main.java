b94a6e0dab0020877ad4738ac74fa3da596d5ce4
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-01-28 18:27:56 +0700

    Solved Kefa and First Steps problem

---
+++ Kefa_and_First_Steps/Main.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static InputReader in;
+    private static PrintWriter out;
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        in = new InputReader(inputStream);
+        out = new PrintWriter(outputStream);
+
+        int n = in.nextInt();
+        int result = 0;
+        int tempResult = 0;
+        int temp = 0;
+        int x = 0;
+        while (n > 0) {
+            x = in.nextInt();
+            result = x >= temp ? result : Math.max(result, tempResult);
+            tempResult = x >= temp ? tempResult + 1 : 1;
+            temp = x;
+            n--;
+        }
+        result = Math.max(result, tempResult);
+        out.println(result);
+
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public char nextChar() {
+            return next().charAt(0);
+        }
+ 
+        public Long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
