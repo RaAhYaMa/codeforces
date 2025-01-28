b6507d82b8afa7cccfb6ce2b88fb0f07098ce102
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-01-28 13:29:04 +0700

    Solved HQ9+ problem

---
+++ HQ9+/Main.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static InputReader in;
+    private static PrintWriter out;
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        in = new InputReader(inputStream);
+        out = new PrintWriter(outputStream);
+
+        String s = in.next();
+
+        int i = 0;
+        for (i=0 ; i < s.length() ; i++) {
+            if (s.charAt(i) == 'H' || s.charAt(i) == 'Q' || s.charAt(i) == '9') {
+                out.println("YES");
+                break;
+            }
+        }
+
+        if (i == s.length()) {
+            out.println("NO");
+        }
+
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public char nextChar() {
+            return next().charAt(0);
+        }
+ 
+        public Long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
