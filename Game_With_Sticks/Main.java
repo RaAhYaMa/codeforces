ae667f0d0a5fb775c2faa2e39a27696010d0f0c8
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-01-28 22:29:11 +0700

    Solved Game With Sticks problem

---
+++ Game_With_Sticks/Main.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static InputReader in;
+    private static PrintWriter out;
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        in = new InputReader(inputStream);
+        out = new PrintWriter(outputStream);
+
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        boolean malvika = Math.min(N, M) % 2 == 0;
+        if (malvika) {
+            out.println("Malvika");
+        }
+        else {
+            out.println("Akshat");
+        }
+
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public char nextChar() {
+            return next().charAt(0);
+        }
+ 
+        public Long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
