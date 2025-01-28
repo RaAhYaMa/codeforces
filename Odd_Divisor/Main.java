bd68468de20c6fc301def662a0b67d6952e2655d
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-01-28 22:38:11 +0700

    Solved Odd Divisor problem

---
+++ Odd_Divisor/Main.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static InputReader in;
+    private static PrintWriter out;
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        in = new InputReader(inputStream);
+        out = new PrintWriter(outputStream);
+
+        int N = in.nextInt();
+
+        long x = 0;
+        while (N > 0) {
+            x = in.nextLong();
+            while (x % 2 != 1) {
+                x = x / 2;
+            }
+            if (x == 1) {
+                out.println("NO");
+            }
+            else {
+                out.println("YES");
+            }
+            N--;
+        }
+
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public char nextChar() {
+            return next().charAt(0);
+        }
+ 
+        public Long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
