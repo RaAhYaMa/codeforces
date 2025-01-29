95799f57c2bf21567c210b3970d024109d88008b
Raden Ahmad Yasin Mahendra <yasinmahendra26@gmail.com>
2025-01-29 17:48:24 +0700

    Solved Chat room problem

---
+++ Chat_room/Main.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static InputReader in;
+    private static PrintWriter out;
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        in = new InputReader(inputStream);
+        out = new PrintWriter(outputStream);
+
+        String s = in.next();
+
+        HashMap<Integer, Character> map = new HashMap<>();
+        map.put(0, 'h'); map.put(1, 'e'); map.put(2, 'l'); map.put(3, 'l'); map.put(4, 'o');
+        int count = 0;
+        int length = s.length();
+        for (int i=0 ; i < length && count < 5 ; i++) {
+            count = s.charAt(i) == map.get(count) ? count + 1 : count;
+        }
+        out.println(count == 5 ? "YES" : "NO");
+
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public char nextChar() {
+            return next().charAt(0);
+        }
+ 
+        public Long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
